5d741d610ef5d1f8da34925b56d6db87610ea286
unknown <messithiago300@gmail.com>
2020-07-15 22:12:57 +0530

    update

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
-    callbackURL: "https://powerful-garden-95030.herokuapp.com/auth/google/secrets",
+    callbackURL: "https://thawing-river-18412.herokuapp.com/auth/google/secrets",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
